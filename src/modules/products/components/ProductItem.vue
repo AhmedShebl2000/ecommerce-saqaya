2cd1fa15c285bc7c574415b0bfb8dc5adfdb6c84
AhmedShebl2000 <bokashalaboka@gmail.com>
2026-04-18 17:33:11 +0200

    refactor(product-item): migrate product item component to Composition API
    - Replaced Options API with <script setup>
    - Converted computed slug to Composition API
    - Replaced  and  with useRoute and useRouter
    - Updated navigation logic to use router.push
    - Removed this usage and simplified method logic

--- src/modules/products/components/ProductItem.vue
+++ src/modules/products/components/ProductItem.vue
@@ -11,39 +11,69 @@
   ></base-product-card>
 </template>
 
-<script>
+<script setup>
 import { slugify } from "@/mixins/slugify";
 import { useProductsStore } from "../store/products";
+import { computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
 
-export default {
-  props: [
-    "id",
-    "images",
-    "title",
-    "price",
-    "discountPercentage",
-    "rating",
-    "ratingCount",
-  ],
-  computed: {
-    productsStore() {
-      return useProductsStore();
-    },
-    slug() {
-      return slugify(this.title);
-    },
-  },
-  methods: {
-    handleOpenProduct() {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-      const targetPath = `/products/${this.id}/${this.slug}`;
-      this.productsStore.setSelectedProduct(this.id);
+const route = useRoute();
+const router = useRouter();
 
-      if (this.$route.path === targetPath) return;
-      this.$router.push(`/products/${this.id}/${this.slug}`);
-    },
-  },
-};
+const productsStore = useProductsStore();
+
+const props = defineProps([
+  "id",
+  "images",
+  "title",
+  "price",
+  "discountPercentage",
+  "rating",
+  "ratingCount",
+]);
+
+const slug = computed(() => {
+  return slugify(props.title);
+});
+
+function handleOpenProduct() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+  const targetPath = `/products/${props.id}/${slug.value}`;
+  productsStore.setSelectedProduct(props.id);
+
+  if (route.path === targetPath) return;
+  router.push(`/products/${props.id}/${slug.value}`);
+}
+
+// export default {
+//   props: [
+//     "id",
+//     "images",
+//     "title",
+//     "price",
+//     "discountPercentage",
+//     "rating",
+//     "ratingCount",
+//   ],
+//   computed: {
+//     productsStore() {
+//       return useProductsStore();
+//     },
+//     slug() {
+//       return slugify(this.title);
+//     },
+//   },
+//   methods: {
+//     handleOpenProduct() {
+//       window.scrollTo({ top: 0, behavior: "smooth" });
+//       const targetPath = `/products/${this.id}/${this.slug}`;
+//       this.productsStore.setSelectedProduct(this.id);
+
+//       if (this.$route.path === targetPath) return;
+//       this.$router.push(`/products/${this.id}/${this.slug}`);
+//     },
+//   },
+// };
 </script>
 
 <style lang="scss" scoped></style>
